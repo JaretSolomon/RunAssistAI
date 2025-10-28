39aec84f6c22662d0cb265c9a7c2578186455380
Teragram04 <margaretparsons3304@gmail.com>
2025-10-28 16:48:18 -0500

    RUN-24 Add multiple routes to login page

--- GUI.py
+++ GUI.py
@@ -3,7 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+@app.route("/login")
+def login():
     return "<p>Welcome to RunAssist!</p>"
 
 @app.route("/logrun")
