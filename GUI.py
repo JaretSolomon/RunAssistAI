a83b21d8f0824d23477e46efb935b896cb73f51d
Teragram04 <margaretparsons3304@gmail.com>
2025-10-28 16:42:10 -0500

    RUN-24 Add stats and log run pages

--- GUI.py
+++ GUI.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Welcome to RunAssist!</p>"
+
+@app.route("/logrun")
+def log_run():
+    return "<p>Log a Run!</p>"
+
+@app.route("/viewstats")
+def view_stats():
+    return "<p>View Statistics!</p>"
